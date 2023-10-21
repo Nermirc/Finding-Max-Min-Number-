fae9da20a229050ca062493da1c9048c7b06a52d
Nermirc <141329241+Nermirc@users.noreply.github.com>
2023-10-21 12:48:05 +0300

    Finding Max-Min-Number
    calculating max min number by using if else conditions

---
+++ Finding Max Min Number/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+  /*
+    int a,b,c;
+    printf("Uc farkli tam sayi giriniz.");
+    scanf("%d%d%d",&a,&b,&c);
+
+    if(a>b && a>c){
+        printf("%d sayisi en buyuk sayidir",a);
+    }
+    else if(b<a && b<c){
+        printf("%d sayisi en kucuk sayidir.",b);
+    }
+    else{
+        printf("Herhangi bir karsilastirma soz konusu degildir."); */
+        //daha dogru bir sekilde olusturulan fonksiyon
+        int a,b,c;
+        int min,max;
+        printf("Uc farkli tam sayi giriniz.");
+        scanf("%d%d%d",&a,&b,&c);
+        max=a;
+        if(b>max){
+            max=b;
+        }
+        if(c>max){
+            max=c;
+        }
+        printf("Maximum degerimiz %d\n",max);
+
+        min=a;
+
+        if(b<min){
+            min=b;
+        }
+        if(c<min){
+            min=c;
+        }
+        printf("Minimum degerimiz %d\n",min);
+
+
+    return 0 ;
+}
